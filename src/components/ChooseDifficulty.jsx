fedb8906f52e9d0a5e4e57146bef121f8b9a07b7
MidStein <deepakchauhan19feb@gmail.com>
2023-06-07 17:47:30 +0530

    Add ChooseDifficulty component
    Shows interface for choosing easy or hard difficulty.

---
+++ src/components/ChooseDifficulty.jsx
@@ -0,0 +1,21 @@
+const ChooseDifficulty = ({ selectRandIllus }) => {
+  return (
+    <div className="ChooseDifficulty">
+      <div className="diffSelectionBox">
+        <div className="diffQuery">Choose Difficulty</div>
+
+        <div className="difficulties">
+          <button onClick={() => { selectRandIllus('easy') }}>
+            Easy (fewer people)
+          </button>
+          <button onClick={() => { selectRandIllus('hard') }}>
+            Hard (more people)
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChooseDifficulty;
+
