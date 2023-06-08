1097df8f1cada97189e083ae43b3ad028f16fb3f
MidStein <deepakchauhan19feb@gmail.com>
2023-06-08 17:56:08 +0530

    Add firebase
    Register app on firebase, access config in app and initialize using
    `firebase init`

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAchZjRZiqu5WG5hUwo9AUZKkWQ7YXpF64",
+  authDomain: "where-s-waldo-4eed0.firebaseapp.com",
+  projectId: "where-s-waldo-4eed0",
+  storageBucket: "where-s-waldo-4eed0.appspot.com",
+  messagingSenderId: "1096195747848",
+  appId: "1:1096195747848:web:e3058c8f7ef8eeec0421bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
